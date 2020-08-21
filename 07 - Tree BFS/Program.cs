2b63b32a99059339e667a8959aa7266900c1b0d9
rrandol3@fau.edu <rrandol3@fau.edu>
2020-08-21 07:33:22 -0400

    BFS leetcode questions added

--- 07 - Tree BFS/Program.cs
+++ 07 - Tree BFS/Program.cs
@@ -8,28 +8,19 @@ namespace _07___Tree_BFS
     {
         static void Main(string[] args)
         {
-            TreeNode tree = new TreeNode(1);
-            tree.left = new TreeNode(2);
-            tree.right = new TreeNode(3);
-            tree.left.left = new TreeNode(4);
-            tree.left.right = new TreeNode(5);
-            tree.right.left = new TreeNode(6);
-            tree.right.right = new TreeNode(7);
-            Stopwatch stopwatch1 = new Stopwatch();
-            stopwatch1.Start();
-            var list = TreePractice.LevelOrderTraversal(tree);
-            stopwatch1.Stop();
-            Console.WriteLine($"Without for loop: {stopwatch1.Elapsed}");
-
-            Stopwatch stopwatch2 = new Stopwatch();
-            stopwatch2.Start();
-            var list2 = TreePractice.LevelOrderTraversal2(tree);
-            stopwatch2.Stop();
-            Console.WriteLine($"With for loop: {stopwatch2.Elapsed}");
-            //foreach (var item in list)
-            //{
-            //    Console.Write($"{item} ");
-            //}
+           
         }
+
+        //Binary Tree Level Order Traversal (easy) https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+        //Reverse Level Order Traversal (easy) https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+
+        //Zigzag Traversal (medium) https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+
+        //Level Averages in a Binary Tree (easy) https://leetcode.com/problems/average-of-levels-in-binary-tree/
+
+        //Minimum Depth of a Binary Tree (easy) https://leetcode.com/problems/minimum-depth-of-binary-tree/
+
+        //Level Order Successor (easy) 
     }
 }
