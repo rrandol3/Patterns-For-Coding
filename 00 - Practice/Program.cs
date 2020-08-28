79ce25c2c6e374ec2baa7e26ca6ad707fa9403ed
rrandol3@fau.edu <rrandol3@fau.edu>
2020-08-28 16:58:18 -0400

    keep grinding

--- 00 - Practice/Program.cs
+++ 00 - Practice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _00___Practice
 {
@@ -7,6 +8,64 @@ namespace _00___Practice
         static void Main(string[] args)
         {
             
+        }
+
+        public static List<int> TopoSort(int vertices, int[][] edges)
+        {
+            List<int> sortedOrder = new List<int>();
+            if (vertices <= 0)
+            {
+                return sortedOrder;
+            }
+
+            Dictionary<int, int> inDegree = new Dictionary<int, int>();
+            Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
+            for (int i = 0; i < vertices; i++)
+            {
+                inDegree.Add(i, 0);
+                graph.Add(i, new List<int>());
+            }
+
+            for (int i = 0; i < edges.Length; i++)
+            {
+                var src = edges[i][0];
+                var dst = edges[i][0];
+                inDegree[dst]++;
+                graph[src].Add(dst);
+            }
+
+            Queue<int> sources = new Queue<int>();
+            foreach (var item in inDegree)
+            {
+                if (item.Value == 0)
+                {
+                    sources.Enqueue(item.Key);
+                }
+            }
+
+            while (sources.Count != 0)
+            {
+                var src = sources.Dequeue();
+                sortedOrder.Add(src);
+                var children = graph[src];
+                foreach (var child in children)
+                {
+                    inDegree[child]--;
+                    if (inDegree[child] == 0)
+                    {
+                        sources.Enqueue(child);
+                    }
+                }
+            }
+
+            if (sortedOrder.Count != vertices)
+            {
+                throw new Exception("Graph has Cycle");
+            }
+
+            var test = int.Parse('s')
+
+            return sortedOrder;
         }
     }
 }
---
+++ 30 - Facebook/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _30___Facebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1,2,3 };
+            var test = SubarraySum(nums, 3);
+        }
+
+        //680. Valid Palindrome II https://leetcode.com/problems/valid-palindrome-ii/
+        //input: abca -> acba
+        //Notes: did not get right
+        public static bool ValidPalindrome(string s)
+        {
+            int low = 0;
+            int high = s.Length - 1;
+            int count = 1;
+            int idx = -1;
+            while (low <= high)
+            {
+                if (s[low] == s[high])
+                {
+                    low++;
+                    high--;
+                }
+                else
+                {
+                    if (count == 0)
+                    {
+                        return false;
+                    }
+                    if (s[low] == s[idx])
+                    {
+                        low++;
+                    }
+                    else
+                    {
+                        idx = high;
+                        high--;
+                        low++;
+                        count--;
+                    }
+                }
+            }
+            return true;
+        }
+
+        //560. Subarray Sum Equals K https://leetcode.com/problems/subarray-sum-equals-k/
+        public static int SubarraySum(int[] nums, int k)
+        {
+            if (nums.Length == 0)
+            {
+                return -1;
+            }
+            else if (nums.Length == 1)
+            {
+                if (nums[0] == k)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            int count = 0;
+            int start = 0;
+            int sum = 0;
+            for (int end = 0; end < nums.Length; end++)
+            {
+                sum += nums[end];
+                while (sum >= k)
+                {
+                    if (sum == k)
+                    {
+                        count++;
+                    }
+                    sum -= nums[start];
+                    start++;
+                }
+            }
+            return count;
+        }
+    }
+}
